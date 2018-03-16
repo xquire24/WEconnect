5a960fb1e67fb950d54f4fffba914723211d804c
xquire24 <xquire24@gmail.com>
2018-03-15 23:51:38 -0700

    enter dummy data for businesses.js

---
+++ server/models/businesses.js
@@ -0,0 +1,20 @@
+const businesses = [{
+  id: 1,
+  name: 'andela',
+  image: 'andela.jpg',
+  description: 'a software development company changing the face of africa',
+  category: 'ICT',
+  location: 'lagos',
+  email: 'andela@andela.com',
+},
+{
+  id: 2,
+  name: 'tradby',
+  image: 'andela.jpg',
+  description: 'a software development company changing the face of africa',
+  category: 'ICT',
+  location: 'lagos',
+  email: 'andela@andela.com',
+}];
+
+export default businesses;
