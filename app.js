c6b7bea62bc9c56b06dd950bd1cb07ebcc1c111b
xquire24 <xquire24@gmail.com>
2018-03-16 05:32:59 -0700

    import express validator in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import expressValidator from 'express-validator';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import routes from './server/routes/route';
@@ -12,6 +13,7 @@ app.use(logger('dev'));
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 routes(app);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
