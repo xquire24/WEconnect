c49302f8ad3e0e38cbfe1ff77a4e2f3611e4af12
xquire24 <xquire24@gmail.com>
2018-03-16 05:00:14 -0700

    add validator to registerBusiness API

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,8 @@
 import Business from '../controller/businessController';
+import ValidateBusiness from '../middleware/businessValidator';
 
 export default (app) => {
   // Business API endpoints
-  app.post('/api/v1/businesses', Business.registerBusiness);
+  app.post('/api/v1/businesses', ValidateBusiness.registerBusinessValidator, Business.registerBusiness);
 };
 
