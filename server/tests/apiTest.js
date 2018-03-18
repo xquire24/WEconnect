eaad2f601757700ea1478350e07c4880cb6c625a
xquire24 <xquire24@gmail.com>
2018-03-17 23:35:52 -0700

    rename and refactor test files

---
+++ server/tests/apiTest.js
@@ -0,0 +1,33 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+// test for app API
+describe('apiApp testing', () => {
+  // For Homepage route
+  it('Should return an Ok status code of 200', (done) => {
+    chai.request(app)
+      .get('/api/v1')
+      .end((err, res) => {
+        expect(res.body).to.have.property('message');
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.a('object');
+        done();
+      });
+  });
+  // For the default route
+  it('Should return an Ok status code of 200', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.body).to.have.property('message');
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.a('object');
+        done();
+      });
+  });
+});
+
