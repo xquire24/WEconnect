ab1b40ebd654f9e6dd060baf331bd705e7fa19b7
xquire24 <xquire24@gmail.com>
2018-03-18 00:51:08 -0700

    add updateBusiness method

--- server/controller/businessController.js
+++ server/controller/businessController.js
@@ -22,5 +22,29 @@ class Business {
       error: false
     });
   }
+  /**
+   * @returns {Object} updateBusiness
+   * @param {*} req
+   * @param {*} res
+   */
+  static updateBusiness(req, res) {
+    for (let i = 0; i < businesses.length; i += 1) {
+      if (businesses[i].id === parseInt(req.params.businessId, 10)) {
+        businesses[i].name = req.body.name;
+        businesses[i].description = req.body.description;
+        businesses[i].location = req.body.location;
+        businesses[i].category = req.body.category;
+        businesses[i].email = req.body.email;
+        return res.json({
+          message: 'Updated Successfully',
+          error: false
+        });
+      }
+    }
+    return res.status(400).json({
+      message: 'Business not found',
+      error: true
+    });
+  }
 }
 export default Business;
