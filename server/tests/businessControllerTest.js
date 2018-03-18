11174c78668c0f77915db4d5d462eeb5fbf09679
xquire24 <xquire24@gmail.com>
2018-03-17 23:35:11 -0700

    rename and refactor test files

---
+++ server/tests/businessControllerTest.js
@@ -0,0 +1,48 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import app from '../../app';
+
+const Business = {
+  name: 'Andel',
+  description: 'a software development company changing the face of africa',
+  category: 'ICT',
+  location: 'lagos',
+  email: 'andelare@andela.com',
+};
+
+chai.use(chaiHttp);
+const { expect } = chai;
+// test for register business route and middle ware
+describe('POST businesses/', () => {
+  it('should be able to register a business', (done) => {
+    chai.request(app)
+      .post('/api/v1/businesses')
+      .send(Business)
+      .end((err, res) => {
+        expect(res)
+          .to.have.status(201);
+        expect(res.body)
+          .to.be.a('object');
+        done();
+      });
+  });
+
+  it('should return 400 if no business name', (done) => {
+    chai.request(app)
+      .post('/api/v1/businesses')
+      .send({
+        description: 'Best Ict Resources',
+        location: 'lagos',
+        category: 'ICT',
+        email: 'xquireworld@gmail.com',
+      })
+      .end((err, res) => {
+        expect(res)
+          .to.have.status(400);
+        expect(res.body)
+          .to.be.a('object');
+        done();
+      });
+  });
+});
