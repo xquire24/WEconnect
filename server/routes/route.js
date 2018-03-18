d5e9b1c29ec43e89906acd0b06de8462dc4c6a20
xquire24 <xquire24@gmail.com>
2018-03-18 01:04:34 -0700

    add updateBusiness route

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,5 +4,6 @@ import ValidateBusiness from '../middleware/businessValidator';
 export default (app) => {
   // Business API endpoints
   app.post('/api/v1/businesses', ValidateBusiness.registerBusinessValidator, Business.registerBusiness);
+  app.put('/api/v1/businesses/:businessId', ValidateBusiness.registerBusinessValidator, Business.updateBusiness);
 };
 
