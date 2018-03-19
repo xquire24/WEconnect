d539776fd816ba4ef04cff6b8cfc11b854adb396
xquire24 <xquire24@gmail.com>
2018-03-18 19:15:58 -0700

    Add update validator method

--- server/middleware/businessValidator.js
+++ server/middleware/businessValidator.js
@@ -32,5 +32,27 @@ class ValidateBusiness {
 
     next();
   }
+  /**
+   * @returns {Object} query
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  static updateBusinessValidator(req, res, next) {
+    for (let i = 0; i < businesses.length; i += 1) {
+      if (businesses[i].id === parseInt(req.params.businessId, 10)) {
+        req.check('name', 'Business name is required').notEmpty();
+        req.check('description', 'Description is required').notEmpty();
+        req.check('category', 'Category is required').notEmpty();
+        req.check('location', 'Location is required').notEmpty();
+        req.check('email', 'Email is required').notEmpty();
+        req.check('email', 'Email is not valid').isEmail();
+        const errors = req.validationErrors();
+        if (errors) { return errorMessage(res, errors[0].msg); }
+      }
+    }
+    next();
+  }
 }
+
 export default ValidateBusiness;
