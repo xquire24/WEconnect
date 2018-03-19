f4a581441fc447c10103946cea1f2d57a9d92d5f
xquire24 <xquire24@gmail.com>
2018-03-18 20:51:09 -0700

    Add API endpoint for Delete business

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,6 +4,7 @@ import ValidateBusiness from '../middleware/businessValidator';
 export default (app) => {
   // Business API endpoints
   app.post('/api/v1/businesses', ValidateBusiness.registerBusinessValidator, Business.registerBusiness);
-  app.put('/api/v1/businesses/:businessId', ValidateBusiness.registerBusinessValidator, Business.updateBusiness);
+  app.put('/api/v1/businesses/:businessId', ValidateBusiness.updateBusinessValidator, Business.updateBusiness);
+  app.delete('/api/v1/businesses/:businessId', Business.removeBusiness);
 };
 
