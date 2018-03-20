f260fc6e7bfbe7cc3f9efbc30e27c25ed50320f2
xquire24 <xquire24@gmail.com>
2018-03-20 11:11:35 +0000

    Edit business model

--- server/models/businesses.js
+++ server/models/businesses.js
@@ -1,20 +1,28 @@
-const businesses = [{
-  id: 1,
-  name: 'andela',
-  image: 'andela.jpg',
-  description: 'a software development company changing the face of africa',
-  category: 'ICT',
-  location: 'lagos',
-  email: 'andela@andela.com',
-},
-{
-  id: 2,
-  name: 'tradby',
-  image: 'andela.jpg',
-  description: 'a software development company changing the face of africa',
-  category: 'ICT',
-  location: 'lagos',
-  email: 'andela@andela.com',
-}];
+const businesses = [
+  {
+    id: 1,
+    name: 'andela',
+    image: 'andela.jpg',
+    description: 'a software development company changing the face of africa',
+    category: 'ICT',
+    location: 'lagos',
+    email: 'andela@andela.com',
+    reviews: [
+      {
+        name: 'charles',
+        review: 'good job'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'tradby',
+    image: 'andela.jpg',
+    description: 'a software development company changing the face of africa',
+    category: 'ICT',
+    location: 'lagos',
+    email: 'andela@andela.com',
+    reviews: []
+  }];
 
 export default businesses;
