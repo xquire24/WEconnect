532746d073ffb3531a8890a520c19643b547c92e
xquire24 <xquire24@gmail.com>
2018-03-22 12:04:46 +0000

    Create user validator and add registerUser method

---
+++ server/middleware/userValidator.js
@@ -0,0 +1,32 @@
+
+const errorMessage = (res, message) => res.status(400).json({
+  message,
+  error: true
+});
+/**
+ *
+ *
+ * @class ValidateUser
+ */
+class ValidateUser {
+  /**
+   *
+   *
+   * @param {any} req
+   * @param {any} res
+   * @param {any} next
+   * @returns {object} userSignup
+   */
+  static userSignup(req, res, next) {
+    req.check('username', 'Username is required').notEmpty();
+    req.check('email', 'Use a valid email').isEmail();
+    req.check('password', 'A minimum of 6 characters is required').isLength({ min: 6 });
+
+    const error = req.validationErrors();
+    if (error) {
+      return errorMessage(res, error[0].msg);
+    }
+    next();
+  }
+}
+export default ValidateUser;
