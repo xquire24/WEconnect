a678ee34a133b2d9f13f443feed53280542d1c56
xquire24 <xquire24@gmail.com>
2018-03-22 08:04:12 +0000

    Add validator method for location and category

--- server/middleware/businessValidator.js
+++ server/middleware/businessValidator.js
@@ -1,5 +1,6 @@
 import businesses from '../models/businesses';
 
+
 const errorMessage = (res, message) => res.status(400).json({
   message,
   error: true
@@ -57,6 +58,47 @@ class ValidateBusiness {
   static businessReviewValidator(req, res, next) {
     req.check('name', 'Name is required').notEmpty();
     req.check('review', 'Review is required').notEmpty();
+    const errors = req.validationErrors();
+    if (errors) { return errorMessage(res, errors[0].msg); }
+
+    next();
+  }
+  /**
+   * @returns {Object} query
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  static queryBusinessByLocationOrCategory(req, res, next) {
+    const { location, category } = req.query;
+    const filter = [];
+    if (location || category) {
+      if (location) {
+        for (let i = 0; i < businesses.length; i += 1) {
+          if (location.toLowerCase() === businesses[i].location.toLowerCase()) {
+            filter.push(businesses[i]);
+          }
+        }
+      }
+      if (category) {
+        for (let i = 0; i < businesses.length; i += 1) {
+          if (category.toLowerCase() === businesses[i].category.toLowerCase()) {
+            filter.push(businesses[i]);
+          }
+        }
+      }
+      if (filter.length === 0) {
+        return res.status(404).json({
+          message: 'Business not found',
+          error: true
+        });
+      }
+      return res.status(200).json({
+        Search_result: filter,
+        message: 'Success',
+        error: false
+      });
+    }
     const errors = req.validationErrors();
     if (errors) { return errorMessage(res, errors[0].msg); }
 
