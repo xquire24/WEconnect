3ef566f2c5508f68b663b5750e43d96b732f21b5
xquire24 <xquire24@gmail.com>
2018-03-22 14:27:03 +0000

    Add login method

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -22,5 +22,27 @@ class User {
       error: false
     });
   }
+  /**
+   * @returns {Object} loginUser
+   * @param {*} req
+   * @param {*} res
+   */
+  static loginUser(req, res) {
+    const { email, password } = req.body;
+    users.forEach((user) => {
+      if (
+        email === user.email && password === user.password
+      ) {
+        return res.status(200).json({
+          message: 'Login successfull',
+          error: false
+        });
+      }
+    });
+    res.status(400).json({
+      message: 'Error logining in',
+      error: true
+    });
+  }
 }
 export default User;
