059555bec67e8357ea2477dfabbafcf108f0bb5b
xquire24 <xquire24@gmail.com>
2018-03-22 11:51:19 +0000

    Create user controller and add registerUser method

---
+++ server/controller/userController.js
@@ -0,0 +1,26 @@
+import users from '../models/users';
+
+
+/**
+ * @class User
+*/
+class User {
+  /**
+   * @returns {Object} registerUser
+   * @param {*} req
+   * @param {*} res
+   */
+  static registerUser(req, res) {
+    users.push({
+      id: users.length + 1,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.json({
+      message: 'Registered Successfully',
+      error: false
+    });
+  }
+}
+export default User;
